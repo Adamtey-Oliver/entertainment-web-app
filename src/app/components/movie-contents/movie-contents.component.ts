f9aa1d41c16f3fdd2f0dde43cd8bedea1eaaed13
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-04-16 14:56:28 +0100

    Create movie-contents.component.ts

---
+++ src/app/components/movie-contents/movie-contents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-contents',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-contents.component.html',
+  styleUrl: './movie-contents.component.css'
+})
+export class MovieContentsComponent {
+
+}
