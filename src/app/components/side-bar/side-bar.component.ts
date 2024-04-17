6aaffc0c9d646abff746ed3592e53b3b7eb67d10
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-04-17 12:23:37 +0100

    Update side-bar.component.ts

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  constructor(private router: Router) {}
+
+  logout() {
+    this.router.navigate(['login']);
+  }
 
 }
