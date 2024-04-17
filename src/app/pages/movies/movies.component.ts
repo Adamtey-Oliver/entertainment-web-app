780e8f457324c6617ca8e3636ed6088d1dd12626
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-04-17 08:08:24 +0100

    Update movies.component.ts

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import { MovieContentsComponent } from '../../components/movie-contents/movie-contents.component';
+import {NgForOf, NgIf} from "@angular/common";
+import { SideBarComponent } from '../../components/side-bar/side-bar.component';
+import {TrendingComponent} from "../../components/trending/trending.component";
+import {MoviesInterface} from "../../interface/movies-interface";
+import {MovieServiceService} from "../../services/movie-service.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [
+    MoviesComponent,
+    NgForOf,
+    SideBarComponent,
+    TrendingComponent,
+    MovieContentsComponent,
+    NgIf,
+    FormsModule,
+  ],
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+  styleUrl: './movies.component.css',
 })
 export class MoviesComponent {
-
+  movieList: MoviesInterface[] = [];
+  constructor(private rs: MovieServiceService) {}
+  ngOnInit(): void {
+    this.rs.getAllMovies().subscribe((response: MoviesInterface[]) => {
+      this.movieList = response.filter((mov) => mov.category.includes('Movie'));
+    });
+  }
+  userSearch:any;
+  search() {
+    if (this.userSearch == "") {
+      this.ngOnInit();
+    }else{
+      this.movieList = this.movieList.filter(res =>{
+        return res.title.toLocaleLowerCase().match(this.userSearch.toLocaleLowerCase())
+      } )
+    }
+  }
 }
